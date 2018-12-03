24e0bae1daf36d4492b52c8df14022ba293a4372
Gregory Hill <gregorydhill@outlook.com>
2018-12-03 10:53:00 +0000

    add txt plugins
    Signed-off-by: Gregory Hill <gregorydhill@outlook.com>

--- .zsh_plugins.sh
+++
@@ -1,8 +0,0 @@
-source /home/greg/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
-fpath+=( /home/greg/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions )
-source /home/greg/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
-fpath+=( /home/greg/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting )
-source /home/greg/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search/zsh-history-substring-search.plugin.zsh
-fpath+=( /home/greg/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-history-substring-search )
-source /home/greg/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-subnixr-SLASH-minimal/minimal.zsh
-fpath+=( /home/greg/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-subnixr-SLASH-minimal )
